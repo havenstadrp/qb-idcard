84aa3fc2ba3866b3e17625173a4706c5b0cad177
bedar89 <bedar89@gmail.com>
2021-10-04 07:26:52 +0000

    adapted to BE id card and driver license

--- html/assets/js/app.js
+++ html/assets/js/app.js
@@ -1,7 +1,9 @@
 window.addEventListener('message', function( event ) {
   var sex = event.data.information.gender
-  $('#card').show();
+  
   if (event.data.nui == 'idcard') {
+    $('#drivercard').hide();
+    $('#card').show();
     $("#name").html(event.data.information.name);
     $("#sex").html(event.data.information.gender);
     $("#dob").html(event.data.information.dob);
@@ -11,20 +13,27 @@ window.addEventListener('message', function( event ) {
     $("body").css("color","#000");
     }
     if (event.data.nui == 'driverlicense') {
-      $("#name").html(event.data.information.name);
-      $("#dob").html(event.data.information.dob);
-      $("#type").html(event.data.information.type);
-      $("#nationality").html('');
-      $("#fingerprint").html('');
-      $("#sex").html('');
-      $("body").css("color","#fff");
-      $('#card').css('background', 'url("/html/assets/images/driverlicense.png")');
+      $('#drivercard').show();
+      $('#card').hide();
+      $("#lastname").html(event.data.information.lastname);
+      $("#firstname").html(event.data.information.firstname);
+      $("#driverdob").html(event.data.information.driverdob);
+      $("#drivertypemotor").html(event.data.information.drivertypemotor);
+      $("#drivertypecar").html(event.data.information.drivertypecar);
+      $("#drivertypetruck").html(event.data.information.drivertypetruck);
+      
+      
+      $("#drivernationality").html('');
+      $("#driverfingerprint").html('');
+      $("#driversex").html('');
+      $("body").css("color","#ededed");
+      $('#drivercard').css('background', 'url("/html/assets/images/driverlicense.png")');
       }  
     if ( sex == '0' ) {
       $('#card').css('background', 'url("/html/assets/images/idcard.png")');
       $('#sex').text('male');
     } else if ( sex == '1' ) {
-      $('#card').css('background', 'url("/html/assets/images/idcard2.png")');
+      $('#card').css('background', 'url("/html/assets/images/idcard.png")');
       $('#sex').text('female');
     }
 
@@ -37,4 +46,5 @@ var charCode = event.keyCode || event.which;
 if (charCode == 27) {
   $.post(`https://qb-idcard/escape`)
   $('#card').hide();
+  $('#drivercard').hide();
 }};
--- server.lua
+++ server.lua
@@ -11,11 +11,20 @@ end)
 QBCore.Functions.CreateCallback('qb-idcard:server:openInformation', function(source, cb, item)
     local inf = {
         ['name'] = item["info"]["lastname"] .. ' ' .. item["info"]["firstname"],
+        ['lastname'] = item["info"]["lastname"],
+        ['firstname'] = item["info"]["firstname"],
         ['gender'] = item["info"]["gender"],
         ['dob'] = item["info"]["birthdate"],
         ['nationality'] = item["info"]["nationality"],
         ['type'] = item["info"]["type"],
         ['fingerprint'] = item["info"]["fingerprint"],
+        -- driver
+        ['lastname'] = '1. ' .. item["info"]["lastname"],
+        ['firstname'] = '2. ' .. item["info"]["firstname"],
+        ['driverdob'] = '3. ' .. item["info"]["birthdate"] .. ' Havenstad', -- TODO change city naar hetgene ze invullen
+        ['drivertypemotor'] = '4. 🏍️ ' .. item["info"]["type"],
+        ['drivertypecar'] = '5. 🚗 ' .. item["info"]["type"],
+        ['drivertypetruck'] = '6. 🚚 ',
     }
     cb(inf)
 end)
